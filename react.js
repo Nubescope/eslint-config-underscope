3ee44b70f53b3817a4c871f31ddea5b5d15bd343
Ignacio Olaciregui <ignacioola@gmail.com>
2019-11-13 09:40:28 -0300

    Split react config

---
+++ react.js
@@ -0,0 +1,38 @@
+const OFF = 0;
+const WARNING = 1;
+const ERROR = 2;
+
+module.exports = {
+  extends: [
+    './node.js',
+    'plugin:react/recommended',
+    'prettier/react',
+  ],
+  plugins: [
+    'react',
+    'react-hooks',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: {
+    'react/display-name': OFF,
+    'react/no-multi-comp': [WARNING, { 'ignoreStateless': true }],
+    'react/no-unused-prop-types': OFF,
+    'react/prop-types': OFF,
+    'react/jsx-filename-extension': OFF,
+    'react/jsx-uses-react': ERROR,
+    'react/jsx-uses-vars': ERROR,
+    'react/react-in-jsx-scope': ERROR,
+    'react/destructuring-assignment': [OFF, 'always'],
+    'react-hooks/rules-of-hooks': ERROR,
+    'react-hooks/exhaustive-deps': WARNING,
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
